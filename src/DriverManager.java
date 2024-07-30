df1cfd3875217caafdb93a9f3716ca924550d196
Ayantika Das <ayantika1607@gmail.com>
2024-07-30 23:56:06 +0530

    first

---
+++ src/DriverManager.java
@@ -0,0 +1,20 @@
+
+import java.sql.Connection;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcoraclethinDESKTOPB2LQN4P1521orcl, String ankita, String mitra) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
